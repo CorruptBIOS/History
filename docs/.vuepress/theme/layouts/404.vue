91bafe3c8e9b33fed625d16226aa92986a84616d
CorruptBIOS <jamesnicol0127@gmail.com>
2021-12-17 15:16:02 +0000

    Update docs/.vuepress/dist/assets/css/0.styles.af01a4e3.css, docs/.vuepress/dist/assets/js/3.30039b8f.js, docs/.vuepress/dist/assets/js/4.dc8dec5e.js, docs/.vuepress/dist/assets/js/7.45ec8123.js, docs/.vuepress/dist/assets/js/8.6e0b04bd.js, docs/.vuepress/dist/assets/js/analytics.js, docs/.vuepress/dist/index.html, docs/.vuepress/theme/layouts/404.vue, docs/.vuepress/theme/layouts/Layout.vue, docs/.vuepress/theme/components/Home.vue, docs/.vuepress/dist/assets/js/app.2f0da9bf.js

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="theme-container">
     <div class="theme-default-content">
-      <img src="https://images.rbxcdn.com/9281912c23312bc0d08ab750afa588cc.png" alt="404"  width="512">
+      <img
+        src="https://images.rbxcdn.com/9281912c23312bc0d08ab750afa588cc.png"
+        alt="404"
+        width="512"
+      />
 
       <h1>404</h1>
       <blockquote>{{ getMsg() }}</blockquote>
@@ -18,14 +22,14 @@ const msgs = [
   `Looks like we've got a leak.`,
   `Someone divided by zero.`,
   `That's Un-Four-unate.`,
-  `We cannot find what you are looking for so instead we show you this blank page.`
-]
+  `We cannot find what you are looking for so instead we show you this blank page.`,
+];
 
 export default {
   methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
-  }
-}
+    getMsg() {
+      return msgs[Math.floor(Math.random() * msgs.length)];
+    },
+  },
+};
 </script>
